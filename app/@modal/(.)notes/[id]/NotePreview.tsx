2ff05d64dec584ccfdf8a1e08d1079e650f3576e
ShumyloYana <zolotushko1@gmail.com>
2025-08-07 17:15:51 +0300

    app/@modal/(.)notes/[id]/NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/NotePreview.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.tsx
@@ -1,5 +1,5 @@
 // import { fetchNoteById } from "@/lib/api";
-import css from "./NotePreview.module.css";
+import css from "../../../../components/NotePreview/NotePreview.module.css";
 
 type Note = {
   id: string;
