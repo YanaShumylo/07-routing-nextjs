fd4a26736326ef36f7e12066c0d86cee3256dd5c
ShumyloYana <zolotushko1@gmail.com>
2025-08-07 15:20:59 +0300

    updated Promise

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,12 +1,22 @@
 import NoteClient from './NoteClient';
 import { fetchNoteById } from "../../../../lib/api";
 
-interface Props {
-  params: { id: string };
-};
+// interface Props {
+//   params: { id: string };
+// };
 
-export default async function NoteModalPage({ params }: Props) {
-  const note = await fetchNoteById(params.id);
+// export default async function NoteModalPage({ params }: Props) {
+//   const note = await fetchNoteById(params.id);
+//   return <NoteClient note={note} />;
+// }
+
+export default async function NoteModalPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
   return <NoteClient note={note} />;
 }
 
@@ -14,7 +24,6 @@ export default async function NoteModalPage({ params }: Props) {
 //   const note = await fetchNoteById(params.id);
 //   return <NoteClient note={note} />;
 // }
-
 // export default NoteModalPage;
 
 // export default async function NoteModalPage({
