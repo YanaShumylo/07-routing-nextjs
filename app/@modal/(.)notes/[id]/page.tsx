02f71692e81bf241bd921b38bbeb4c9f3ab26d92
ShumyloYana <zolotushko1@gmail.com>
2025-08-07 12:21:12 +0300

    updated page delete interface

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,13 +1,22 @@
 import NoteClient from './NoteClient';
 import { fetchNoteById } from "../../../../lib/api";
 
-interface Props {
-  params: { id: string };
-};
+// interface Props {
+//   params: { id: string };
+// };
 
-const NoteModalPage = async ({ params }: Props) => {
+// const NoteModalPage = async ({ params }: Props) => {
+//   const note = await fetchNoteById(params.id);
+//   return <NoteClient note={note} />;
+// }
+
+// export default NoteModalPage;
+
+export default async function NoteModalPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const note = await fetchNoteById(params.id);
   return <NoteClient note={note} />;
 }
-
-export default NoteModalPage;
