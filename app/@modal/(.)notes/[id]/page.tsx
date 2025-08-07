cab82180e8df1685e3ad323e88acd9cba9b10458
ShumyloYana <zolotushko1@gmail.com>
2025-08-07 15:02:18 +0300

    updated page NoteModalPage

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,9 +1,14 @@
 import NoteClient from './NoteClient';
 import { fetchNoteById } from "../../../../lib/api";
 
-// interface Props {
-//   params: { id: string };
-// };
+interface Props {
+  params: { id: string };
+};
+
+export default async function NoteModalPage({ params }: Props) {
+  const note = await fetchNoteById(params.id);
+  return <NoteClient note={note} />;
+}
 
 // const NoteModalPage = async ({ params }: Props) => {
 //   const note = await fetchNoteById(params.id);
@@ -12,11 +17,11 @@ import { fetchNoteById } from "../../../../lib/api";
 
 // export default NoteModalPage;
 
-export default async function NoteModalPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const note = await fetchNoteById(params.id);
-  return <NoteClient note={note} />;
-}
+// export default async function NoteModalPage({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const note = await fetchNoteById(params.id);
+//   return <NoteClient note={note} />;
+// }
